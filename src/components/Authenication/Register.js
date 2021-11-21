648c27589cc04f07a0337193dc168d308a02c105
keshawn0998 <keshawn@contenda.co>
2021-11-21 08:14:32 -0500

    trying out the heroku endpoint

--- src/components/Authenication/Register.js
+++ src/components/Authenication/Register.js
@@ -45,10 +45,10 @@ export default class Register extends Component {
       loading: true
     })
     axios
-      .post("http://localhost:5000/register", this.state.user)
+      .post("https://movie-app-be.herokuapp.com/register", this.state.user)
       .then((res) => {
         console.log("hello")
-        axios.post(`http://localhost:5000/login`, {"email":this.state.user.email,"password":this.state.user.password}).then((res) => {
+        axios.post(`https://movie-app-be.herokuapp.com/login`, {"email":this.state.user.email,"password":this.state.user.password}).then((res) => {
         this.setState({...this.state,loading:false})
         localStorage.setItem(`token`, res.data.token);
         localStorage.setItem(`id`, res.data.userid);
